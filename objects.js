aaa89380548af996b2d2bbd48680bbee913cea64
Mike Bakker <equanoxproductions@gmail.com>
2018-01-15 03:39:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ function updatePlaylist(playlist, artistName, songTitles) {
   playlist[artistName] = songTitles
   return playlist
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
